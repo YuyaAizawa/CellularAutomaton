d0450b4861e338214ded77ac5c078d3aae1a099a
YuyaAizawa <yaizawa@sa.cs.titech.ac.jp>
2018-12-18 23:49:25 +0900

    リファクタリング elm-reference導入など

--- src/CellularAutomaton.elm
+++ src/CellularAutomaton.elm
@@ -6,6 +6,9 @@ import Html exposing (Html, div, input, label, text, table, tr, td, h1, br, butt
 import Html.Attributes exposing (type_, for, value, name, id, checked)
 import Html.Events exposing (onInput, onClick)
 
+import Reference exposing (Reference)
+import Reference.List
+
 import Styles
 
 main =
@@ -38,11 +41,6 @@ type EdgeType
   | AlwaysAlive
   | Loop
 
-flip state =
-  case state of
-    Alive -> Dead
-    Dead -> Alive
-
 init : Model
 init =
   { origin = [Dead, Alive, Dead, Dead]
@@ -52,64 +50,65 @@ init =
   , edge = AlwaysDead
   }
 
+flip state =
+  case state of
+    Alive -> Dead
+    Dead -> Alive
+
+toState bool =
+  if bool then Alive else Dead
+
 ruleFromInt : Int -> Rule
 ruleFromInt index =
   ( \pgen ->
     case pgen of
-      ( Dead,  Dead,  Dead) -> if bitAt 0 index then Alive else Dead
-      ( Dead,  Dead, Alive) -> if bitAt 1 index then Alive else Dead
-      ( Dead, Alive,  Dead) -> if bitAt 2 index then Alive else Dead
-      ( Dead, Alive, Alive) -> if bitAt 3 index then Alive else Dead
-      (Alive,  Dead,  Dead) -> if bitAt 4 index then Alive else Dead
-      (Alive,  Dead, Alive) -> if bitAt 5 index then Alive else Dead
-      (Alive, Alive,  Dead) -> if bitAt 6 index then Alive else Dead
-      (Alive, Alive, Alive) -> if bitAt 7 index then Alive else Dead
+      ( Dead,  Dead,  Dead) -> bitTest 0 index
+      ( Dead,  Dead, Alive) -> bitTest 1 index
+      ( Dead, Alive,  Dead) -> bitTest 2 index
+      ( Dead, Alive, Alive) -> bitTest 3 index
+      (Alive,  Dead,  Dead) -> bitTest 4 index
+      (Alive,  Dead, Alive) -> bitTest 5 index
+      (Alive, Alive,  Dead) -> bitTest 6 index
+      (Alive, Alive, Alive) -> bitTest 7 index
   )
 
-bitAt : Int -> Int -> Bool
-bitAt i index =
-  index
-    |> Bitwise.shiftRightBy i
-    |> Bitwise.and 1
-    |> (==) 1
+bitTest : Int -> Int -> State
+bitTest int index =
+  let
+    bool =
+      index
+        |> Bitwise.shiftRightBy int
+        |> Bitwise.and 1
+        |> (==) 1
+  in
+    if bool then Alive else Dead
+
 
 ---- UPDATE ----
 
 type Msg
-  = ColumnChange String
-  | GenerationChange String
+  = ColumnChange Int
+  | GenerationChange Int
   | EdgeChange EdgeType
-  | RuleChange String
+  | RuleChange Int
   | DetailVisibility
   | RuleFlip Int
-  | OriginFlip Int
+  | OriginFlip (Reference State (List State))
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    ColumnChange str ->
-      let
-        nextModel =
-          str
-            |> String.toInt
-            |> resize model
-      in
-      nextModel
+    ColumnChange columns ->
+      resize model columns
 
-    GenerationChange str ->
-      let
-        nextGeneration =
-          str
-            |> String.toInt
-            |> Maybe.withDefault model.generations
-      in
-      { model | generations = nextGeneration }
+    GenerationChange generations ->
+      { model | generations = generations }
 
     EdgeChange edge ->
       { model | edge = edge }
 
     RuleChange rule ->
-      { model | rule = rule |> String.toInt |> Maybe.withDefault model.rule }
+      { model | rule = rule }
 
     DetailVisibility ->
       { model | ruleDetail = not model.ruleDetail }
@@ -117,23 +116,17 @@ update msg model =
     RuleFlip bit ->
       { model | rule = model.rule |> Bitwise.xor (Bitwise.shiftLeftBy bit 1) }
 
-    OriginFlip index ->
-      let
-        origin =
-          model.origin
-            |> List.indexedMap (\i -> \s -> if i == index then flip s else s)
-      in
-      { model | origin = origin }
+    OriginFlip ref ->
+      { model | origin = ref |> Reference.modify flip |> Reference.root }
 
-resize : Model -> Maybe Int -> Model
+resize : Model -> Int -> Model
 resize model size =
   let currentSize = model.origin |> List.length in
-  let nextSize = size |> Maybe.withDefault currentSize in
   let
     nextOrigin =
-      if currentSize < nextSize
-      then List.append model.origin (Dead |> List.repeat (nextSize - currentSize))
-      else List.take nextSize model.origin
+      if currentSize < size
+      then List.append model.origin (Dead |> List.repeat (size - currentSize))
+      else List.take size model.origin
   in
     { model | origin = nextOrigin }
 
@@ -145,51 +138,51 @@ view : Model -> Html Msg
 view model =
   div [] ( List.concat
     [ [h1[][text "せるお～とまとん"]]
-    , numberInput "ルール #" "rule" model.rule 0 255
-        |> (List.map << Html.map) RuleChange
+    , numberInput "ルール #" "rule" RuleChange model.rule 0 255
     , [button[onClick DetailVisibility][text "詳細"]]
     , ruleDetailView model.ruleDetail model.rule
     , [br[][]]
-    , numberInput "セル数" "cells" (model.origin |> List.length) 1 100
-        |> (List.map << Html.map) ColumnChange
+    , numberInput "セル数" "cells" ColumnChange (model.origin |> List.length) 1 100
     , [br[][]]
-    , numberInput "世代数" "generations" model.generations 1 300
-        |> (List.map << Html.map) GenerationChange
+    , numberInput "世代数" "generations" GenerationChange model.generations 1 300
     , [br[][]]
-    , radioButtons "末端処理" "edge" model.edge
+    , radioButtons "末端処理" "edge" EdgeChange model.edge
         [ ("死亡", AlwaysDead)
         , ("生存", AlwaysAlive)
         , ("末端をループ", Loop)
         ]
-        |> (List.map << Html.map) EdgeChange
-
     , [ originView model.origin ]
     , [ descendantsView model.origin model.edge (ruleFromInt model.rule) model.generations ]
     ]
   )
 
-numberInput : String -> String -> Int -> Int -> Int -> List (Html String)
-numberInput title name value min max =
+numberInput : String -> String -> (Int -> msg) -> Int -> Int -> Int -> List (Html msg)
+numberInput title name toMsg value min max =
   [ label [ for name ][ text title ]
   , input
       [ type_ "number"
-      , Html.Attributes.value (value |> String.fromInt)
-      , Html.Attributes.min (min |> String.fromInt)
-      , Html.Attributes.max (max |> String.fromInt)
-      , onInput identity ][]
+      , Html.Attributes.value <| String.fromInt <| value
+      , Html.Attributes.min <| String.fromInt <| min
+      , Html.Attributes.max <| String.fromInt <| max
+      , onInput (String.toInt >> Maybe.withDefault value >> toMsg) ][]
   ]
 
-radioButtons : String -> String -> msg -> List (String, msg) -> List (Html msg)
-radioButtons title group selected candidates =
+radioButtons : String -> String -> (a -> msg) -> a -> List (String, a) -> List (Html msg)
+radioButtons title group toMsg selected candidates  =
   (::)
    (label [ for group ][ text title ])
    (candidates
-    |> List.map (radioButton group selected)
+    |> List.map (radioButton group toMsg selected)
     |> List.concat)
 
-radioButton : String -> msg -> (String, msg) -> List (Html msg)
-radioButton group selected (name_, msg) =
-  [ input [ type_ "radio", name group, id name_, onClick msg, checked (msg == selected)][]
+radioButton : String -> (a -> msg) -> a -> (String, a) -> List (Html msg)
+radioButton group toMsg selected (name_, candidate) =
+  [ input
+    [ type_ "radio"
+    , name group
+    , id name_
+    , onClick (candidate |> toMsg)
+    , checked (candidate == selected)][]
   , label [ for name_ ][ text name_ ]
   ]
 
@@ -214,32 +207,33 @@ editableRule ruleIndex bit =
     div Styles.ruleEditor
       [ table Styles.resultTable
         [ tr[]
-          [ td(cellStyle (bitAt 0 bit))[]
-          , td(cellStyle (bitAt 1 bit))[]
-          , td(cellStyle (bitAt 2 bit))[] ] ]
+          [ td(Styles.cell ++ (bitTest 0 bit |> toStyle))[]
+          , td(Styles.cell ++ (bitTest 1 bit |> toStyle))[]
+          , td(Styles.cell ++ (bitTest 2 bit |> toStyle))[]
+          ]
+        ]
       , text "↓"
       , table Styles.originTable
-        [tr[][ td(onClick (RuleFlip bit) :: cellStyle (bitAt bit ruleIndex))[]]]
+        [tr[][ td(
+          Styles.cell
+          ++ [onClick (RuleFlip bit)]
+          ++ (bitTest bit ruleIndex |> toStyle))[]
+        ]]
       ]
-
-
 
 originView : List State -> Html Msg
 originView origin =
   let
-    cellView : Int -> State -> Html Msg
-    cellView index state =
+    cellView : Reference State (List State) -> Html Msg
+    cellView ref =
       td (List.concat
         [ Styles.cell
-        , case state of
-            Alive -> Styles.alive
-            Dead -> Styles.dead
-        , [ onClick (OriginFlip index) ]
+        , Reference.this ref |> toStyle
+        , [ onClick (OriginFlip ref) ]
         ]) []
   in
-    table
-      Styles.originTable
-      ( origin |> List.indexedMap cellView )
+    table Styles.originTable
+      (Reference.top origin |> Reference.List.unwrap cellView)
 
 descendantsView : List State -> EdgeType -> Rule -> Int -> Html Msg
 descendantsView origin edgeType rule limit =
@@ -247,69 +241,54 @@ descendantsView origin edgeType rule limit =
     step : List State -> List State
     step gen =
       let
-        getLterm cells =
+        getTerm farSide =
           case edgeType of
             AlwaysDead -> Dead
             AlwaysAlive -> Alive
-            Loop ->
-              cells
-                |> List.reverse
-                |> List.head
-                |> Maybe.withDefault Dead
+            Loop -> farSide |> Maybe.withDefault Dead
       in
-      let
-        getRterm cells =
-          case edgeType of
-            AlwaysDead -> Dead
-            AlwaysAlive -> Alive
-            Loop ->
-              cells
-                |> List.head
-                |> Maybe.withDefault Dead
-      in
-
-      let
-        inner ngen l c pgen =
-          case pgen |> List.head of
-            Nothing ->
-              rule (l, c, getRterm gen) :: ngen
-                |> List.reverse
-            Just r ->
-              inner ((rule (l, c, r)) :: ngen) c r (pgen |> List.tail |> Maybe.withDefault [])
-      in
-        case gen of
-          [] ->
-            []
-          c :: [] ->
-            inner [] (getLterm gen) c []
-          l :: c :: rest ->
-            inner [] (getLterm gen) l (c :: rest)
+      let lterm = getTerm <| List.head <| List.reverse <| gen in
+      let rterm = getTerm <| List.head <| gen in
+        [lterm] ++ gen ++ [rterm]
+          |> window3
+          |> List.map rule
   in
 
   let
     generationView : List State -> Html Msg
     generationView cells =
       let
-        cellView cell =
-          td (List.concat
-          [ Styles.cell
-          , case cell of
-              Alive -> Styles.alive
-              Dead -> Styles.dead
-          , []
-          ]) []
+        cellView state =
+          td (Styles.cell ++ (state |> toStyle)) []
       in
         tr [] ( cells
           |> List.map cellView )
   in
 
   let
-    inner trs lim gen =
+    descendantsView_ trs lim gen =
       if lim == 0
       then
         trs |> List.reverse
       else
-        inner (generationView gen :: trs) (lim - 1) (step gen)
+        descendantsView_ (generationView gen :: trs) (lim - 1) (step gen)
   in
-    inner [] limit origin
+    descendantsView_ [] limit origin
       |> table Styles.resultTable
+
+toStyle state =
+      case state of
+        Alive -> Styles.alive
+        Dead -> Styles.dead
+
+window3 : List a -> List (a, a, a)
+window3 list =
+  let
+    window3_ : List (a, a, a) -> List a -> List (a, a, a)
+    window3_ result rest =
+      case rest of
+        hd1 :: hd2 :: hd3 :: tl ->
+          window3_ ((hd1, hd2, hd3) :: result) (hd2 :: hd3 :: tl)
+        _ -> result
+  in
+    window3_ [] list |> List.reverse
